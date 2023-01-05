8d21f61755b03983766b3ce45cda39ae89a515db
Vincentdm31 <vincent.dimarco1@gmail.com>
2023-01-05 09:39:18 +0100

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in navigator) {
+    try {
+      const registration = await navigator.serviceWorker.register("/sw.js", {
+        scope: "/",
+      });
+      if (registration.installing) {
+        console.log("Installation du service worker en cours");
+      } else if (registration.waiting) {
+        console.log("Service worker installé");
+      } else if (registration.active) {
+        console.log("Service worker actif");
+      }
+    } catch (error) {
+      console.error(`L'enregistrement a échoué : ${error}`);
+    }
+  }
+};
+
+// …
+
+registerServiceWorker();
