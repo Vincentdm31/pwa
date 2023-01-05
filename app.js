41ef7c752937e62597ef243beda6d67dc93c4eea
Vincentdm31 <vincent.dimarco1@gmail.com>
2023-01-05 10:43:42 +0100

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const registerServiceWorker = async () => {
   if ("serviceWorker" in navigator) {
     try {
       const registration = await navigator.serviceWorker.register(
-        "/pwa/sw.js",
+        "https://vincentdm31.github.io/pwa/sw.js",
         {
           scope: "https://vincentdm31.github.io/pwa/",
         }
