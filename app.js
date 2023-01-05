0fd38172b37f4bf60d5917888c12a7e3bff794c2
Vincentdm31 <vincent.dimarco1@gmail.com>
2023-01-05 10:58:46 +0100

    fx

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   registerServiceWorker();
+
+  var status = document.getElementById("status");
+
+  function updateOnlineStatus(event) {
+    var condition = navigator.onLine ? "online" : "offline";
+
+    status.className = condition;
+    status.innerHTML = condition.toUpperCase();
+  }
+  window.addEventListener("online", updateOnlineStatus);
+  window.addEventListener("offline", updateOnlineStatus);
 });
 
 const registerServiceWorker = async () => {
